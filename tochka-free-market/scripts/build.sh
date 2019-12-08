0982dee786b528eaa1d63f575ab5a0b3dc36366a
x0rzkov <x0rzkov@gmail.com>
2019-12-08 15:12:25 +0100

    add back tochka-free-market

---
+++ tochka-free-market/scripts/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# go get -u github.com/go-redis/redis
+# go get github.com/Masterminds/glide
+# yes no | glide create
+# glide install --strip-vendor
+
+go get -u github.com/kardianos/govendor
+go get -u golang.org/x/time/rate
+go get -u github.com/gocraft/web
+go get -u github.com/go-redis/redis
+
+go build
+
+cp settings.json.example settings.json
+
+ls -l
